553413cf5bb5fe86de99a94ec0151792ad58019e
Renatis <ismagilov89@gmail.com>
2020-08-18 14:18:13 +0300

    updated yaml files, Controller

--- src/main/java/com/home/b2/microservice/bookingservice/b2microservicebookingservice/Controller.java
+++ src/main/java/com/home/b2/microservice/bookingservice/b2microservicebookingservice/Controller.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.Instant;
 
 @RestController
+//@RequestMapping("/env")
 public class Controller {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "Server is alive. Current time: " + Instant.now();
+        return "Booking Server is alive. Current time: " + Instant.now();
+    }
+
+    @GetMapping("/")
+    String hello() {
+        return "Hello from booking service.";
     }
 }
